3b0c5ee89aa882ba61b0d7e2efa7ee8825cd7912
bibekoli <github@bibekoli.com>
2023-09-16 07:38:24 +0545

    integrate mongodb

---
+++ src/modules/mongodb.js
@@ -0,0 +1,19 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const MongoDBConfig = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+};
+
+let client = null;
+let db = null;
+
+export default async function ConnectToDatabase() {
+  if (!client || !db) {
+    client = new MongoClient(process.env.MONGODB_URL, MongoDBConfig);
+    await client.connect();
+    db = client.db(process.env.MONGODB_NAME);
+  }
+  return { client, db };
+}
