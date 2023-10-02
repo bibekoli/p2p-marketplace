781f83ef7a324cd4d2dba1509a49450bbf17adcd
bibekoli <github@bibekoli.com>
2023-10-02 22:20:59 +0545

    test: created api route to create new listing

---
+++ src/app/api/CreateAd/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import ConnectToDatabase from "@/modules/mongodb";
+
+export async function POST(request) {
+  try {
+    const { db } = await ConnectToDatabase();
+    const items = await db.collection("Items");
+    const data = await request.json();
+    const response = await items.insertOne(data);
+    return NextResponse.json(response);
+  }
+  catch (error) {
+    return NextResponse.error(error);
+  }
+}
