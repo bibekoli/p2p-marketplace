8b9e7172ff077cd28e6fab72636bab23b704e1a2
bibekoli <github@bibekoli.com>
2023-10-04 18:11:17 +0545

    updated item server page to handle notfound items

--- src/app/item/[id]/page.jsx
+++ src/app/item/[id]/page.jsx
@@ -1,15 +1,25 @@
+import ItemNotFound from "./404";
 import ItemClient from "./client";
 
 async function getItem(id) {
-  const res = await fetch(process.env.NEXT_PUBLIC_URL + "/api/GetItem?id=" + id);
-  const json = await res.json();
-  return json;
+  try{
+    const res = await fetch(process.env.NEXT_PUBLIC_URL + "/api/GetItem?id=" + id);
+    const json = await res.json();
+    return json;
+  }
+  catch(e) {
+    return {};
+  }
 }
 
 
 export default async function Item({ params }) {
   const { id } = params;
   const item = await getItem(id);
+
+  if (Object.keys(item).length === 0) {
+    return <ItemNotFound />;
+  }
 
   return <ItemClient item={item} />;
 }
