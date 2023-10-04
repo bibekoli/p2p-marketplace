0b7930cc09c65a8325ddd6a0c2c0a2c8f5718ee2
bibekoli <github@bibekoli.com>
2023-10-04 18:12:21 +0545

    sample item page with image carousel and desc

--- src/app/item/[id]/client.jsx
+++ src/app/item/[id]/client.jsx
@@ -1,14 +1,21 @@
 "use client";
 
+import ImageCarousel from "@/components/ImageCarousel";
 import { useState } from "react";
 
 export default function ItemClient({ item }) {
-  const [n, setn] = useState(1);
-  return (
-    <div>
-    <h1>{n}</h1>
+  console.log(item);
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
-    <button onClick={() => setn(n + 1)}>+</button>
+  return (
+    <div className="flex flex-col md:flex-row max-w-screen-xl mx-auto">
+      <div className={`flex flex-col md:w-1/2 m-4 relative rounded-xl`}>
+        <ImageCarousel images={item.images} activeImageIndex={activeImageIndex} setActiveImageIndex={setActiveImageIndex} />
+      </div>
+      <div className="flex flex-col md:w-1/2 m-4">
+        <h1 className={`text-3xl font-[800]`}>{item.name}</h1>
+        <p className={`text-xl font-[500]`}>{item.description}</p>
+      </div>
     </div>
   )
 }
