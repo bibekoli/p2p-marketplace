dfbabd61daa79c3e65c0ce3169d6937a6af04b0d
bibekoli <github@bibekoli.com>
2023-10-08 19:50:49 +0545

    get all: get seller name with product info

--- src/app/api/GetData/route.js
+++ src/app/api/GetData/route.js
@@ -1,10 +1,42 @@
 import { NextResponse } from "next/server";
 import ConnectToDatabase from "@/modules/mongodb";
 
-export async function GET(request) {
+export async function GET() {
   const { db } = await ConnectToDatabase();
   const items = await db.collection("Items");
   
-  const allItems = await items.find({ visibility: "public" }, { projection: { visibility: 0 } }).toArray();
+  const allItems = await items.aggregate([
+    {
+      $match: { visibility: "public" }
+    },
+    {
+      $lookup: {
+        from: "Users",
+        localField: "seller",
+        foreignField: "_id",
+        as: "sellerInfo"
+      }
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        description: 1,
+        images: 1,
+        condition: 1,
+        category: 1,
+        price: 1,
+        delivery: 1,
+        my_location: 1,
+        status: 1,
+        seller: 1,
+        seller_name: { $arrayElemAt: ["$sellerInfo.name", 0] },
+        keywords: 1,
+        created_at: 1,
+      }
+    }
+  ])
+  .toArray();
+
   return NextResponse.json(allItems);
 }
