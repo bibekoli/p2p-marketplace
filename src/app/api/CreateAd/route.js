7fd2ea0c89aa09c2ce7a8a63db26f9a9ec533f3b
bibekoli <github@bibekoli.com>
2023-10-08 19:01:12 +0545

    create ad api: check if user is authenticated

--- src/app/api/CreateAd/route.js
+++ src/app/api/CreateAd/route.js
@@ -1,15 +1,19 @@
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
 import ConnectToDatabase from "@/modules/mongodb";
 
 export async function POST(request) {
   try {
+    const user = await getServerSession();
+    if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
     const { db } = await ConnectToDatabase();
     const items = await db.collection("Items");
     const data = await request.json();
 
-    const seller = await db.collection("Users").findOne({ email: data.seller });
+    const seller = await db.collection("Users").findOne({ email: data.tempSellerRef });
     data.seller = seller._id;
-
+    delete data.tempSellerRef;
     
     const response = await items.insertOne(data);
     return NextResponse.json({
@@ -17,6 +21,6 @@ export async function POST(request) {
     });
   }
   catch (error) {
-    return NextResponse.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
