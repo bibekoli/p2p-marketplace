1d0d798493081f3a85a961e062c9f40a40104c37
bibekoli <github@bibekoli.com>
2023-10-30 11:38:54 +0545

    added functionality to increment views count

--- src/app/api/GetItem/route.js
+++ src/app/api/GetItem/route.js
@@ -12,7 +12,14 @@ export async function GET(request) {
   const { db } = await ConnectToDatabase();
   const items = await db.collection("Items");
   
-  const item = await items.findOne({ _id: new ObjectId(id), visibility: "public" }, { projection: { visibility: 0 } });
+  const item = await items.findOneAndUpdate({ 
+    _id: new ObjectId(id), 
+    visibility: "public" 
+  }, { $inc: { views: 1 } }, { 
+    projection: { visibility: 0 },
+    returnDocument: "after"
+  });
+
   if (!item) {
     return NextResponse.json({}, { status: 404 });
   }
