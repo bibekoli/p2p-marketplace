06497770c560afcdb364122538950284c5f92043
bibekoli <github@bibekoli.com>
2023-11-02 21:30:36 +0545

    get related products

--- src/app/api/GetItem/route.js
+++ src/app/api/GetItem/route.js
@@ -15,6 +15,8 @@ export async function GET(request) {
   const users = await db.collection("Users");
   const user = await getServerSession();
 
+  const response = {};
+
   const item = await items.findOneAndUpdate({ 
     _id: new ObjectId(id),
   }, {
@@ -26,20 +28,63 @@ export async function GET(request) {
   if (!item) {
     return NextResponse.json({}, { status: 404 });
   }
+  if (item.visibility == "private") {
+    return NextResponse.json({});
+  }
 
+  // getting seller name and product ownership
   const sellerRes = await users.findOne({ _id: new ObjectId(item.seller) });
   item.seller_name = sellerRes.name;
-
   if (user) {
     const userSearch = await users.findOne({ email: user.user.email });
     item.selfOwned = item.seller.toString() == userSearch._id.toString();
-    return NextResponse.json(item);
   }
   else {
     item.selfOwned = false;
-    if (item.visibility == "private") {
-      return NextResponse.json({});
-    }
-    return NextResponse.json(item);
   }
+
+  // getting 24 related products based on same category, keywords, title words, description words (in that order) if no at least 24 products found, then it will get 24 random products
+  // exclude status: "sold" and visibility: "private" and current item
+  const related = await items.aggregate([
+    {
+      $match: {
+        $and: [
+          { _id: { $ne: new ObjectId(id) } },
+          { status: { $ne: "sold" } },
+          { visibility: { $ne: "private" } },
+          { $or: [
+            { category: item.category },
+            { keywords: { $in: item.keywords } },
+            { name: { $in: item.name.split(" ") } },
+            { description: { $in: item.description.split(" ") } },
+          ] }
+        ]
+      }
+    },
+    { $sample: { size: 24 } }
+  ]).toArray();
+
+  if (related.length < 24) {
+    // excluding current item and related items
+    // const random = await items.aggregate([{ $sample: { size: 24 - related.length } }]).toArray();
+    const random = await items.aggregate([
+      {
+        $match: {
+          $and: [
+            { _id: { $ne: new ObjectId(id) } },
+            { _id: { $nin: related.map(item => item._id) } },
+            { status: { $ne: "sold" } },
+            { visibility: { $ne: "private" } },
+          ]
+        }
+      },
+      { $sample: { size: 24 - related.length } }
+    ]).toArray();
+    related.push(...random);
+  }
+
+  response.item = item;
+  response.related = related;
+
+  return NextResponse.json(response);
 }
