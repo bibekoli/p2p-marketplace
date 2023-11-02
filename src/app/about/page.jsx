eb0f5e1b3b1d20c26008ce498fe2e0b0cf49d15a
bibekoli <github@bibekoli.com>
2023-11-02 22:37:37 +0545

    add faq and about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,40 @@
 export default function AboutUs() {
   return (
     <div className="max-w-screen-xl mx-auto mt-[80px] p-4">
-      about us here
+      <h1 className="text-2xl font-bold">About Us</h1>
+      <main className="leading-8">
+        <section className="my-4">
+          <p className="text-gray-700 mt-2"> Imagine a world where you can effortlessly connect with others who share your passions, interests, and unique offerings. That&apos;s precisely what BechnuParyo aims to accomplish. Our story is one of cutting out the clutter, embracing simplicity, and celebrating the human touch in every transaction. </p>
+        </section>
+        <section className="my-4">
+          <h2 className="text-xl font-bold">What Makes Us Different</h2>
+          <ul className="list-disc ml-8 list-inside text-gray-700 mt-2">
+            <li>
+              <span className="text-lg font-semibold">No Middleman</span><br />Say goodbye to hefty commissions and hidden fees. With BechnuParyo, you get to keep what&apos;s rightfully yours.
+            </li>
+            <br />
+            <li>
+              <span className="text-lg font-semibold">Personal Connections</span><br />Connect directly with your buyers or sellers and build trust. BechnuParyo is a space where genuine relationships are formed.
+            </li>
+            <br />
+            <li>
+              <span className="text-lg font-semibold">Local or Global</span><br />Whether you&apos;re looking for a hidden gem in your neighborhood or seeking a rare find from across the globe, BechnuParyo brings the world to your doorstep.
+            </li>
+            <br />
+            <li>
+              <span className="text-lg font-semibold">User-Centric Design</span><br />Our user-friendly platform is designed with you in mind. We&apos;ve stripped away the complexities to make your experience smooth and enjoyable.
+            </li>
+            <br />
+            <li>
+              <span className="text-lg font-semibold">Safety and Security</span><br />Rest easy with our secure messaging system and vigilant moderation. We&apos;re committed to ensuring that your interactions are safe and secure.
+            </li>
+            <br />
+            <li>
+              <span className="text-lg font-semibold">Green Initiatives</span><br />We&apos;re not just a marketplace; we&apos;re stewards of the environment. Join us in promoting sustainable practices and responsible consumption.
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,7 +1,23 @@
 export default function FAQ() {
   return (
     <div className="max-w-screen-xl mx-auto mt-[80px] p-4">
-      faq here
+      <h1 className="text-2xl font-bold">Frequently Asked Questions</h1>
+      <main className="leading-8 mt-8">
+        <div className="mt-2 space-y-8">
+          <div className="border-b pb-2">
+            <h3 className="text-xl font-semibold">What is BechnuParyo?</h3>
+            <p className="text-gray-700">BechnuParyo is a peer-to-peer marketplace where individuals can buy and sell items directly with each other, without the involvement of a third party.</p>
+          </div>
+          <div className="border-b pb-2">
+            <h3 className="text-xl font-semibold">How can I get started?</h3>
+            <p className="text-gray-700">To get started, create an account, list your items for sale, or start browsing to make a purchase. It&apos;s that simple!</p>
+          </div>
+          <div className="border-b pb-2">
+            <h3 className="text-xl font-semibold">Is BechnuParyo free to use?</h3>
+            <p className="text-gray-700">Yes, BechnuParyo is free to use. We do not charge any listing fees or commissions.</p>
+          </div>
+        </div>  
+      </main>
     </div>
   );
 }
