587fa4c9099c2be881b57c25d87d4c6f52443149
bibekoli <github@bibekoli.com>
2023-11-09 00:23:21 +0545

    update algorithm

--- src/app/api/GetItem/route.js
+++ src/app/api/GetItem/route.js
@@ -43,45 +43,57 @@ export async function GET(request) {
     item.selfOwned = false;
   }
 
-  // getting 12 related products based on same category, keywords, title words, description words (in that order) if no at least 12 products found, then it will get 12 random products
-  // exclude status: "sold" and visibility: "private" and current item
-  const related = await items.aggregate([
-    {
-      $match: {
-        $and: [
-          { _id: { $ne: new ObjectId(id) } },
-          { status: { $ne: "sold" } },
-          { visibility: { $ne: "private" } },
-          { $or: [
-            { category: item.category },
-            // { keywords: { $in: item.keywords } },
-            { name: { $in: item.name.split(" ") } },
-            // { description: { $in: item.description.split(" ") } },
-          ] }
-        ]
-      }
-    },
-    { $sample: { size: 12 } }
-  ]).toArray();
+  // get all items to calculate related items using Collaborative Filtering algorithm
+  // compare current item with all other items, check comparision factors and calculate similarity score:
+  // - same category: +3
+  // - each item has array of keywords, each same keyword: +1
+  // - same words in name: +1 for each word
+  // - same seller: +1
 
-  if (related.length < 12) {
-    // excluding current item and related items
-    // const random = await items.aggregate([{ $sample: { size: 12 - related.length } }]).toArray();
-    const random = await items.aggregate([
-      {
-        $match: {
-          $and: [
-            { _id: { $ne: new ObjectId(id) } },
-            { _id: { $nin: related.map(item => item._id) } },
-            { status: { $ne: "sold" } },
-            { visibility: { $ne: "private" } },
-          ]
-        }
-      },
-      { $sample: { size: 12 - related.length } }
-    ]).toArray();
-    related.push(...random);
+  const allItems = await items.find({
+    visibility: "public",
+    status: "available"
+  }).toArray();
+
+  const related = [];
+  for (const items of allItems) {
+    if (items._id.toString() == id) continue;
+    let score = 0;
+    if (items.category == item.category) score += 3;
+    for (const word of items.keywords) {
+      if (item.keywords.includes(word)) score += 1;
+    }
+    for (const word of item.keywords) {
+      if (items.keywords.includes(word)) score += 1;
+    }
+    for (const word of items.name.split(" ")) {
+      if (item.name.split(" ").includes(word)) score += 1;
+    }
+    if (items.seller.toString() == item.seller.toString()) score += 1;
+    if (score > 0) {
+      related.push({
+        ...items,
+        score: score
+      });
+    }
   }
+  related.sort((a, b) => {
+    return b.score - a.score;
+  });
+  related.splice(12);
+
+  // if related is less than 12, fill with random items that are not in related using allItems
+  // if (related.length < 12) {
+  //   const used = [];
+  //   for (const item of related) {
+  //     used.push(item._id.toString());
+  //   }
+  //   for (const item of allItems) {
+  //     if (related.length == 12) break;
+  //     if (used.includes(item._id.toString())) continue;
+  //     related.push(item);
+  //   }
+  // }
 
   response.item = item;
   response.related = related;
